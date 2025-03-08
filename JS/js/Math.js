771b03923d55a50110893169314d88a8041815dc
devanshu <devanshukumawat063@gmail.com>
2025-03-08 13:46:33 +0530

    add

---
+++ JS/js/Math.js
@@ -0,0 +1,23 @@
+/**
+
+JS Math methods 
+ Math.PI
+ Math.Random
+ Round
+ ceil
+ floor
+ Sqrt
+ abs
+
+ */
+
+// console.log(Math.PI);
+// console.log(Math.random() * 100);
+
+// let num = -66.1;
+// console.log(num);
+// console.log(Math.abs(num));
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.sqrt(64));
---
+++ JS/js/timefun.js
@@ -0,0 +1,12 @@
+// 1sec = 1000 milli sec
+
+// setTimeout
+// setInterval
+
+// setTimeout(() => {
+//   console.log("Hello Devanshu 😍");
+// }, 2000);
+
+setInterval(() => {
+  console.log("Hahahahahahah..😂");
+}, 1000);
